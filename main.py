72d8e27d59a0f4e56ba77c7237b0532064e1fc0a
Alan <alannavaseg_02@hotmail.com>
2020-06-01 14:58:21 -0500

    LOgramos que el sprite se mueva en los 4 direcciones

--- main.py
+++ main.py
@@ -12,10 +12,24 @@ class Gatell(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = load_image("assets/gatell.png", True)
         self.rect = self.image.get_rect()
-        self.rect.centerx = WIDTH / 2
-        self.rect.centery = HEIGHT / 2
+        self.rect.centerx = WIDTH / 2 
+        self.rect.centery = ( HEIGHT ) - 100
         self.speed = [0.5, -0.5]
-
+       
+    def actualizar(self, keys):               
+        #UP
+        if keys[pygame.K_a] and self.rect.centerx > 0:    
+           self.rect.centerx  -= 2
+        #DOWN
+        if keys[pygame.K_d] and self.rect.centerx < HEIGHT:
+            self.rect.centerx  += 2
+        #LEFT
+        if keys[pygame.K_w]:
+            self.rect.centery -= 2
+        #RIGHT
+        if keys[pygame.K_s]:
+            self.rect.centery += 2
+        print("Posición es =  x: "+ str( self.rect.centerx) + " y: "+str( self.rect.centery ))
 # Funciones
 def load_image(filename, transparent=False):
     try: 
@@ -35,15 +49,25 @@ def load_image(filename, transparent=False):
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Coronavirus Game. Starring: Dr. Gatell")
-    background_image = load_image('assets/mexico.png', True)
+    
+    background_image = load_image('assets/mexico.png', False)
     drGatell = Gatell()
-    while True:        
+    
+    clock = pygame.time.Clock()
+    while True:
+        time = clock.tick(60)
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
+        keys = pygame.key.get_pressed()
+        
+        drGatell.actualizar(keys)
+      
         screen.blit(background_image, (0, 0))
         screen.blit(drGatell.image, drGatell.rect)
-        pygame.display.flip()
+        
+       ## pygame.display.flip()
+        pygame.display.update()
     return 0
  
 if __name__ == '__main__':
