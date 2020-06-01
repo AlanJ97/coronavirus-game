34de21a2668a080f19d18a36f86246a4df8f3cb5
Alan <alannavaseg_02@hotmail.com>
2020-06-01 15:59:34 -0500

    SE queda en al agregar a varios coronavirus al mismo tiempo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # MODULOS   
 import sys, pygame
+from  time import sleep
 from pygame.locals import *
 
 # Constantes 
@@ -7,6 +8,7 @@ WIDTH = 736
 HEIGHT = 500
 
 # Clases  
+#Clase del presonaje principal
 class Gatell(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -14,23 +16,52 @@ class Gatell(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2 
         self.rect.centery = ( HEIGHT ) - 100
-        self.speed = [0.5, -0.5]
-       
+    
+    #Mover al personaje principal
     def actualizar(self, keys):               
-        #UP
-        if keys[pygame.K_a] and self.rect.centerx > 0:    
-           self.rect.centerx  -= 2
-        #DOWN
-        if keys[pygame.K_d] and self.rect.centerx < HEIGHT:
-            self.rect.centerx  += 2
-        #LEFT
-        if keys[pygame.K_w]:
-            self.rect.centery -= 2
-        #RIGHT
-        if keys[pygame.K_s]:
-            self.rect.centery += 2
-        print("Posición es =  x: "+ str( self.rect.centerx) + " y: "+str( self.rect.centery ))
-# Funciones
+        
+        #Left     
+        if self.rect.left  <= WIDTH and self.rect.left  >= 0   and keys[pygame.K_a]:
+           self.rect.centerx  -= 3
+
+        #Right       
+        if self.rect.right >= 0 and self.rect.right <= WIDTH  and keys[pygame.K_d]:
+            self.rect.centerx  += 3
+
+        #UP       
+        if self.rect.top >= 0 and keys[pygame.K_w]:
+            self.rect.centery -= 3
+
+        #Down
+        if self.rect.bottom  <= HEIGHT and keys[pygame.K_s]:
+            self.rect.centery += 3
+        
+        #print("rect top: "+ str(self.rect.top) + " rect bottom: "+ str(self.rect.bottom))
+        #print("rect right: "+ str(self.rect.right) + " rect left: "+ str(self.rect.left))
+        #print("witdh image: "+ str(self.image.get_width()) + " height image: "+ str(self.image.get_height()))
+        #print("Posición es =  x: "+ str( self.rect.centerx) + " y: "+str( self.rect.centery ))
+
+#Clase de los enemigos del personaje
+class Coronavirus(pygame.sprite.Sprite):
+    def __init__(self, x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = load_image("assets/coronavirus.png", True)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.centery = HEIGHT / 2
+       
+    #Mover al enemigo
+    def mover(self, keys):
+        if self.rect.top >= 0:
+            if keys[K_u]:
+                self.rect.centery -= 3
+        if self.rect.bottom <= HEIGHT:
+            if keys[K_j]:
+                self.rect.centery += 3
+
+
+# Funciones principales
+#carga la imagen de fondo
 def load_image(filename, transparent=False):
     try: 
         image = pygame.image.load(filename)
@@ -45,31 +76,39 @@ def load_image(filename, transparent=False):
     return image
 
 # Ejecutar el programa 
-
 def main():
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Coronavirus Game. Starring: Dr. Gatell")
     
     background_image = load_image('assets/mexico.png', False)
+
     drGatell = Gatell()
+    for i in range(3):
+        coronavirus = Coronavirus(i * 10)
+        sleep(2)
     
     clock = pygame.time.Clock()
+
+    #Ciclo que mantiene abierta la ventana hasta que se decide cerrar
     while True:
         time = clock.tick(60)
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
+        
         keys = pygame.key.get_pressed()
         
         drGatell.actualizar(keys)
+        coronavirus.mover(keys)
       
         screen.blit(background_image, (0, 0))
         screen.blit(drGatell.image, drGatell.rect)
-        
-       ## pygame.display.flip()
+        screen.blit(coronavirus.image, coronavirus.rect)
+               
         pygame.display.update()
     return 0
  
+# Llamada a la funcion que inicia e programa
 if __name__ == '__main__':
     pygame.init()
     main()
